9e64635d99e710e0d308a430afb37e5a702d86c3
Serge Stecenko <sergejs.stecenko@gmail.com>
2024-08-23 19:36:26 +0300

    feat: add UI components

--- src/lib/utils/helpers.ts
+++ src/lib/utils/helpers.ts
@@ -1,1 +1,12 @@
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
 export const HOST = process.env.NEXT_PUBLIC_URL ?? 'http://localhost:3000';
+
+export function isNumber(value: string) {
+  return !isNaN(parseInt(value));
+}
