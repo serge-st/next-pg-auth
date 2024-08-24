7e35bdcb3122c865d483938ddf0bbeeaa1ebfff0
Serge Stecenko <sergejs.stecenko@gmail.com>
2024-08-24 11:24:45 +0300

    refactor(UserPage): improve structure

--- src/app/users/[id]/page.tsx
+++ src/app/users/[id]/page.tsx
@@ -1,11 +1,13 @@
+import { NextPage } from 'next';
+
 interface UserProps {
   params: {
     id: string;
   };
 }
 
-const User = ({ params }: UserProps) => {
-  return <h1 className="w-full text-center text-lg">User {params.id}</h1>;
+const User: NextPage<UserProps> = ({ params: { id } }) => {
+  return <h1 className="w-full text-center text-lg">User {id}</h1>;
 };
 
 export default User;
