e43885cd1bf001f477f80d1572e9e0e3486ed3b1
Serge Stecenko <sergejs.stecenko@gmail.com>
2024-08-25 18:02:54 +0300

    feat(UsersPage): add tabs to separate data

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,5 +1,6 @@
 import { AddUserForm } from '@/components/add-user-form';
 import { apiClient } from '@/lib/api/api-client';
+import { Tabs, TabsList, TabsTrigger, TabsContent } from '@/components/ui/tabs';
 
 const fetchUsers = async () => {
   const result = await apiClient.get('/users');
@@ -20,7 +21,16 @@ export default async function UsersPage() {
   return (
     <>
       <h1 className="w-full text-center text-2xl">User Management</h1>
-      <AddUserForm availableRoles={availableRoles}></AddUserForm>
+      <Tabs defaultValue="list" className="w-[400px]">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="list">User List</TabsTrigger>
+          <TabsTrigger value="form">Add User</TabsTrigger>
+        </TabsList>
+        <TabsContent value="list">Make changes to your account here.</TabsContent>
+        <TabsContent value="form">
+          <AddUserForm availableRoles={availableRoles}></AddUserForm>
+        </TabsContent>
+      </Tabs>
     </>
   );
 }
