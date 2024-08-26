01adb44827fceeda6177097841c6f39e9b70dc5f
Serge Stecenko <sergejs.stecenko@gmail.com>
2024-08-27 01:32:53 +0300

    build: replace bcrypt package

--- src/lib/api/password-utils.ts
+++ src/lib/api/password-utils.ts
@@ -1,6 +1,6 @@
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 
-const SALT_ROUNDS = 10;
+const SALT_ROUNDS = 8;
 
 export async function hashPassword(data: string): Promise<string> {
   const salt = await bcrypt.genSalt(SALT_ROUNDS);
