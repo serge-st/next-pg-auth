bc61dd0c5d7388aefede6e14085134c2263a1103
Serge Stecenko <sergejs.stecenko@gmail.com>
2024-09-03 19:33:57 +0300

    feat(build): check for required env variables

---
+++ env-check.ts
@@ -0,0 +1,32 @@
+import 'dotenv/config';
+import fs from 'fs';
+import dotenv from 'dotenv';
+
+function parseEnvExample(): string[] {
+  const envExample = fs.readFileSync('.env.example', 'utf8');
+  const envVars = dotenv.parse(envExample);
+  return Object.keys(envVars);
+}
+
+function checkEnvVariables(requiredVars: string[]): boolean {
+  const missingVars = requiredVars.filter((varName) => !process.env[varName]);
+
+  if (missingVars.length > 0) {
+    console.error('Missing required environment variables:');
+    missingVars.forEach((varName) => console.error(`- ${varName}`));
+    return false;
+  }
+
+  return true;
+}
+
+function main() {
+  const requiredVars = parseEnvExample();
+  const allVarsPresent = checkEnvVariables(requiredVars);
+
+  if (!allVarsPresent) {
+    process.exit(1);
+  }
+}
+
+main();
